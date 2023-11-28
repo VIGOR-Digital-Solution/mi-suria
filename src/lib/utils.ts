8027202a62c4574d57a318cd94bb10294c22fa02
drfrank66613 <71062416+drfrank66613@users.noreply.github.com>
2023-11-28 19:11:00 +0700

    feat: create util function to generate random array number

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateRandomArray(length: number, min: number, max: number) {
+  const randomArray = [];
+  for (let i = 0; i < length; i++) {
+    const randomNumber = Math.random() * (max - min) + min;
+    randomArray.push(randomNumber);
+  }
+  return randomArray;
 }
