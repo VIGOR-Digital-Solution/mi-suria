498c5acc1b858ecd0fa11e64344ec98d13db3d13
drfrank66613 <71062416+drfrank66613@users.noreply.github.com>
2023-11-28 19:18:09 +0700

    feat: create user layout & pages

--- src/app/(users)/user/page.tsx
+++ src/app/(users)/user/page.tsx
@@ -3,21 +3,6 @@ import { redirect } from "next/navigation";
 import React from "react";
 import SignOut from "./components/sign-out";
 
-export default async function AdminPage() {
-  const { data } = await readUserSession();
-
-  if (!data.session) {
-    return redirect("/auth");
-  }
-
-  if (data.session.user.user_metadata?.role !== "user") {
-    return redirect("/unauthorized");
-  }
-
-  return (
-    <div>
-      <div> Welcome to user page, {data.session.user.email}</div>
-      <SignOut />
-    </div>
-  );
+export default async function UserPage() {
+  return redirect("/user/profile");
 }
