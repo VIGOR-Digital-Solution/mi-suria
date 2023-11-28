0881a996c79879469350e8b5b68b709719825851
drfrank66613 <71062416+drfrank66613@users.noreply.github.com>
2023-11-28 19:17:05 +0700

    feat: create admin layout & pages

--- src/app/(users)/admin/page.tsx
+++ src/app/(users)/admin/page.tsx
@@ -4,20 +4,5 @@ import React from "react";
 import SignOut from "./components/sign-out";
 
 export default async function AdminPage() {
-  const { data } = await readUserSession();
-
-  if (!data.session) {
-    return redirect("/auth");
-  }
-
-  if (data.session.user.user_metadata?.role !== "admin") {
-    return redirect("/unauthorized");
-  }
-
-  return (
-    <div>
-      <div> Welcome to admin page, {data.session.user.email}</div>
-      <SignOut />
-    </div>
-  );
+  return redirect("/admin/profile");
 }
