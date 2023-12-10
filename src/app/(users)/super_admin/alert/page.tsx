08655a31cea264e83b984fa3a1f3a8a98759e27a
Victor Lee <124036643+vigor-victorlee@users.noreply.github.com>
2023-12-11 01:04:01 +0800

    feat: ui enhancement

--- src/app/(users)/super_admin/alert/page.tsx
+++ src/app/(users)/super_admin/alert/page.tsx
@@ -5,7 +5,7 @@ import { columns } from "./components/columns";
 
 export default function AlertPage() {
   return (
-    <div>
+    <div className="p-6">
       <h1 className="text-3xl font-bold mb-5">Alerts</h1>
 
       <DataTable columns={columns} data={alerts} />
--- src/app/(users)/super_admin/device/page.tsx
+++ src/app/(users)/super_admin/device/page.tsx
@@ -5,8 +5,8 @@ import { columns } from "./components/columns";
 
 export default function DevicePage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold mb-5">Devices</h1>
+    <div className="p-6">
+      <h1 className=" text-3xl font-bold mb-5">Devices</h1>
 
       <DataTable columns={columns} data={devices} />
     </div>
