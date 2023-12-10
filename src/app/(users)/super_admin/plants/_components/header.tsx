a8b09028edb5fbd3a58ba04e7827c736d1211ff9
Victor Lee <124036643+vigor-victorlee@users.noreply.github.com>
2023-12-11 01:15:00 +0800

    Update header.tsx

--- src/app/(users)/super_admin/plants/_components/header.tsx
+++ src/app/(users)/super_admin/plants/_components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="items-center flex p-6">
-      <p className="text-3xl font-bold">My Plants</p>
+      <p className="text-3xl font-bold">Plants</p>
     </div>
   );
 }
