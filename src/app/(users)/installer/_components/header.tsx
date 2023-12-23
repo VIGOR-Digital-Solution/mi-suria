96db5bc379f05944cf9f1207eb59a53d09b03a00
drfrank66613 <71062416+drfrank66613@users.noreply.github.com>
2023-12-23 16:51:32 +0700

    feat: add breadcrumbs functionality to header of installer

--- src/app/(users)/installer/_components/header.tsx
+++ src/app/(users)/installer/_components/header.tsx
@@ -1,7 +1,28 @@
+"use client";
+import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+
 export default function Header({ title }: { title: string }) {
+  const pathname = usePathname();
+  const breadcrumbs = pathname.split("/").slice(3);
+
   return (
     <div className="items-center flex p-6">
-      <p className="text-3xl font-bold">{title}</p>
+      <Breadcrumbs size="lg" className="capitalize">
+        <BreadcrumbItem href={`/installer/${title.toLowerCase()}`}>
+          {title}
+        </BreadcrumbItem>
+        {breadcrumbs.map((breadcrumb, index) => {
+          const path = `/${breadcrumbs.slice(0, index + 1).join("/")}`;
+
+          return (
+            <BreadcrumbItem key={path} href={path}>
+              {breadcrumb}
+            </BreadcrumbItem>
+          );
+        })}
+      </Breadcrumbs>
+      {/* <p className="text-3xl font-bold">{title}</p> */}
     </div>
   );
 }
