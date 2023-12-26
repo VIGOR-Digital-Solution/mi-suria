4f35098dfd0a7df4f440981e955208418f46cee4
drfrank66613 <71062416+drfrank66613@users.noreply.github.com>
2023-12-26 09:49:56 +0700

    feat: add email field
    - feat: add relations
    - feat: add user type

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
   jsonb,
   pgTable,
@@ -7,11 +8,21 @@ import {
   uuid,
   varchar,
 } from "drizzle-orm/pg-core";
+import { permission } from "./permission";
+import { plant } from "./plant";
 
 export const user = pgTable("user", {
   id: uuid("id").primaryKey(),
   name: varchar("name").notNull(),
+  email: text("email").notNull(),
   createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
     .defaultNow()
     .notNull(),
 });
+
+export const userRelations = relations(user, ({ one, many }) => ({
+  plants: many(plant),
+  permission: one(permission),
+}));
+
+export type User = typeof user.$inferSelect; // return type when queried
