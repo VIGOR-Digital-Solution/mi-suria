3d62b7fbea4c40f56c2b6c2afcb0474d496938cc
drfrank66613 <71062416+drfrank66613@users.noreply.github.com>
2023-12-26 11:33:46 +0700

    feat(api): modify plants route to include user relation
    include user relation if query param is set to true

--- src/app/api/plants/route.ts
+++ src/app/api/plants/route.ts
@@ -2,8 +2,20 @@ import { db } from "@/db";
 import { plant } from "@/db/schema/plant";
 
 export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const isUser = searchParams.get("is_user");
+  let user: true | undefined = undefined;
+
   try {
-    const plants = await db.query.plant.findMany();
+    if (isUser && isUser === "true") {
+      user = true;
+    }
+
+    const plants = await db.query.plant.findMany({
+      with: {
+        user,
+      },
+    });
 
     return Response.json(plants, {
       status: 200,
