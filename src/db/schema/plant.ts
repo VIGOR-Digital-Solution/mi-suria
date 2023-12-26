c243800ef5954398fee7bd65ead643809724f1c8
drfrank66613 <71062416+drfrank66613@users.noreply.github.com>
2023-12-26 09:50:53 +0700

    feat: add userId field to plant schema
    - feat: add relations
    - feat: add types

--- src/db/schema/plant.ts
+++ src/db/schema/plant.ts
@@ -4,8 +4,11 @@ import {
   serial,
   text,
   timestamp,
+  uuid,
   varchar,
 } from "drizzle-orm/pg-core";
+import { User, user } from "./user";
+import { relations } from "drizzle-orm";
 
 export const plant = pgTable("plant", {
   id: serial("id").primaryKey(),
@@ -14,6 +17,10 @@ export const plant = pgTable("plant", {
   coordinates: jsonb("coordinates")
     .$type<{ lat: number; lng: number }>()
     .notNull(),
+  userId: uuid("user_id").references(() => user.id, {
+    onUpdate: "cascade",
+    onDelete: "cascade",
+  }),
   createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
     .defaultNow()
     .notNull(),
@@ -22,4 +29,19 @@ export const plant = pgTable("plant", {
     .notNull(),
 });
 
+export const plantRelations = relations(plant, ({ one }) => ({
+  user: one(user, {
+    fields: [plant.userId],
+    references: [user.id],
+  }),
+  // createdBy: one(user, {
+  //   fields: [permission.createdBy],
+  //   references: [user.id],
+  // }),
+}));
+
+export type Plant = typeof plant.$inferSelect; // return type when queried
+export type PlantWithUser = {
+  user: User | null;
+} & Plant;
 export type NewPlant = typeof plant.$inferInsert; // insert type
