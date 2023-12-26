9f32dd263cb02ef514b920b9d1cd44af39f418ab
drfrank66613 <71062416+drfrank66613@users.noreply.github.com>
2023-12-26 09:47:45 +0700

    feat: add plants table

--- src/app/(users)/installer/plants/page.tsx
+++ src/app/(users)/installer/plants/page.tsx
@@ -1,8 +1,22 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import { DataTable } from "./_components/table/data-table";
+import { columns } from "./_components/table/columns";
+import { usePlants } from "@/hooks";
 
 export default function AccountsPage() {
+  const { data: plants, isLoading } = usePlants({ isUser: true });
+
+  console.log(plants?.data);
+
+  const tableData = plants?.data || [];
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="space-y-5">
       <Link
@@ -11,6 +25,8 @@ export default function AccountsPage() {
       >
         Create +
       </Link>
+
+      <DataTable columns={columns} data={tableData} />
     </div>
   );
 }
