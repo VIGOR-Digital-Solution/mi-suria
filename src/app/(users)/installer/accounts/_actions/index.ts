095d56101c4bd7352fa44c96a8276e288b3751fa
drfrank66613 <71062416+drfrank66613@users.noreply.github.com>
2024-01-03 17:12:51 +0700

    feat(installer): create change password action

--- src/app/(users)/installer/accounts/_actions/index.ts
+++ src/app/(users)/installer/accounts/_actions/index.ts
@@ -81,6 +81,20 @@ export async function readAccounts() {
     }); // modify based on the related access rule
 }
 
+export async function changePassword(
+  currentPassword: string,
+  newPassword: string
+) {
+  const supabase = await createSupabaseServerClientReadOnly();
+
+  const updateUserResult = await supabase.rpc("change_user_password", {
+    current_plain_password: currentPassword,
+    new_plain_password: newPassword,
+  });
+
+  return JSON.stringify(updateUserResult);
+}
+
 export async function editAccount(
   id: string,
   data: {
